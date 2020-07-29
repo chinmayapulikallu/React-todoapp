0880e0c803d6b444ef4ac8dc776997060e2ef67d
Chinmaya Pulikallu <chinmayapulikallu@gmail.com>
2020-07-28 23:22:51 -0500

    added feature to add new todo list

--- src/Todos.js
+++ src/Todos.js
@@ -5,8 +5,7 @@ const Todos = ({ todos, deleteTodo }) => {
         todos.map(todo => {
             return (
                 <div className="collection-item" key={todo.id}>
-                    <span> {todo.content}</span>
-                    <button onClick={() => {deleteTodo(todo.id)}}>Delete</button>
+                    <span onClick={() => { deleteTodo(todo.id) }}> {todo.content}</span>
                 </div>
             )
         })
